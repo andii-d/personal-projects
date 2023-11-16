20e0b8ad1bfeec509a168ca3e450d7a027d8ce4b
a <104162882+r3strict@users.noreply.github.com>
2023-11-16 15:06:51 +0000

    Update spell_check.py

--- Python Projects/Spell Checker/spell_check.py
+++ Python Projects/Spell Checker/spell_check.py
@@ -1,1 +1,23 @@
+import re
 
+dictionary = set()
+counter = 0
+
+with open("words_alpha.txt") as f:
+    for word in f:
+        dictionary.add(word.strip())
+
+sentence = input("Enter your sentence to spell check: ")
+sentence = sentence.lower().split()
+
+for word in sentence:
+    if word not in dictionary:
+        print(f"Misspelled word: {word}")
+    else:
+        counter += 1
+        pass
+
+if counter >= 1:
+    print("The other words were spelt correctly")
+else:
+    print("No words were spelt correctly")
