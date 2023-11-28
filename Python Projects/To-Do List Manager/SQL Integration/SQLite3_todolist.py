69bc643c37ad45619347b95ac36ccd46d8c9b921
a <104162882+r3strict@users.noreply.github.com>
2023-11-28 10:45:48 +0000

    Update SQLite3_todolist.py

--- Python Projects/To-Do List Manager/SQL Integration/SQLite3_todolist.py
+++ Python Projects/To-Do List Manager/SQL Integration/SQLite3_todolist.py
@@ -1,1 +1,175 @@
-a
+import time
+import os
+import sqlite3
+
+connection = sqlite3.connect("tasks.db")
+cursor = connection.cursor()
+
+cursor.execute("SELECT username FROM usernames")
+
+# uses list comprehension - for each value in the first column selected, fetch
+# it
+usernames = [row[0] for row in cursor.fetchall()]
+
+print(usernames)
+
+class User:
+    def __init__(self, username):
+        self.username = username
+        self.tasks = {}
+        self.task_count = 1
+
+    def addTask(self, task):
+        task_info = {"task": task, "completed": False}
+        self.tasks[self.task_count] = task_info
+        self.task_count += 1
+
+    def completedTask(self, username):
+        with open(f"{username}_completed_tasks.txt", "a") as f:
+            for task_number, task_info in self.tasks.items():
+                task = task_info["task"]
+                completion = task_info["completed"]
+                if completion:
+                    completion_state = "True"
+                    f.write(f"{task_number}: {task}: {completion_state}\n")
+
+    def removeTask(self, task_number):
+        if task_number in self.tasks:
+            removed_task = self.tasks.pop(task_number)
+
+            # Update the task numbers for the remaining tasks
+            updated_tasks = {}
+            for i in range(1, len(self.tasks) + 1):
+                if i < task_number:
+                    updated_tasks[i] = self.tasks[i]
+                else:
+                    updated_tasks[i] = self.tasks[i + 1]
+
+            self.tasks = updated_tasks
+            self.task_count -= 1
+
+            return removed_task
+        else:
+            print(f"Task {task_number} not found.")
+
+    def viewAllTasks(self):
+        for task_number, task_info in self.tasks.items():
+                print(f"Task {task_number}: {task_info['task']}")
+
+    def viewIncompleteTasks(self):
+        for task_number, task_info in self.tasks.items():
+            if not task_info["completed"]:
+                print(f"Task {task_number}: {task_info['task']}")
+
+    def saveTasks(self):
+        with open(f"{self.username}_tasks.txt", "a") as file:
+            for task_number, task_info in self.tasks.items():
+                task = task_info["task"]
+                completion = task_info["completed"]
+                completion_state = "True" if completion else "False"
+                file.write(f"{task_number}: {task}: {completion_state}\n")
+
+    def loadTasks(self):
+        self.tasks = {}
+        try:
+            with open(f"{self.username}_tasks.txt", "r") as file:
+                for line in file:
+                    parts = line.strip().split(": ")
+                    if len(parts) == 3:
+                        task_number, task, completion = parts[0], parts[1].strip(), parts[2]
+                        self.tasks[int(task_number)] = {"task": task, "completed": completion == "True"}
+        except FileNotFoundError:
+            # If the file doesn't exist, create an empty one
+            with open(f"{self.username}_tasks.txt", "w"):
+                pass
+
+    def editTasks(self, task_number_edit, task_new_edited):
+        if task_number_edit in self.tasks:
+            self.tasks[task_number_edit]["task"] = task_new_edited
+            print(f"Task {task_number_edit} updated.")
+        else:
+            print(f"Task {task_number_edit} not found.")
+
+    def toggleTasks(self, task_number_edit):
+        if task_number_edit in self.tasks:
+            current_completion_state = self.tasks[task_number_edit]["completed"]
+            self.tasks[task_number_edit]["completed"] = not current_completion_state
+            print(f"Task {task_number_edit} completed.")
+        else:
+            print(f"Task {task_number_edit} not found. ")
+
+def main():
+    with open('usernames.txt', 'r') as f:
+        usernames = [line.strip() for line in f]
+
+    username = input("Enter your name: ")
+    if username not in usernames:
+        print("\nName does not exist. Creating a new account...\n")
+
+
+    # Create an object of the user's name
+    user = User(username)
+    # Load all the user's tasks if they have any
+    user.loadTasks()
+
+    while True:
+        choice = input("\n1. Add task\n2. Remove task\n3. View tasks\n4. Edit tasks\n5. Toggle completion of tasks\n6. Save and Exit\n")
+
+        if choice == "1":
+            task = input("Enter a task: ")
+            user.addTask(task)
+        elif choice == "2":
+            user.viewAllTasks()
+            while True:
+                try:
+                    task_number = int(input("\nEnter the task number to remove: "))
+                except ValueError:
+                    print("\nEnter a number.\n")
+                    continue
+                else:
+                    user.removeTask(task_number)
+                    break
+        elif choice == "3":
+            if user.tasks:
+                user.viewIncompleteTasks()
+            else:
+                print("No tasks to display.")
+        elif choice == "4":
+            if user.tasks:
+                for task_number, task in user.tasks.items():
+                    print(f"{task_number}: {task}")
+                task_num_edit = int(input("Enter the number of the task you want to edit: "))
+                task_new_edit = input("Enter the new task message: ")
+                user.editTasks(task_num_edit, task_new_edit)
+            else:
+                print("No tasks to display.")
+
+        elif choice == "5":
+            if user.tasks:
+                for task_number, task in user.tasks.items():
+                    print(f"{task_number}: {task}")
+                task_num_edit = int(input("Enter the number of the task you want to complete: "))
+                user.toggleTasks(task_num_edit)
+            else:
+                print("No tasks to display.")
+        elif choice == "6":
+            user.saveTasks()
+            print("Tasks saved. Exiting.")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+# In these following loops:
+    # for key, value in dictionary.items()
+    # The computer iterates through each key and then executes code if the value is also needed.
+
+
+
